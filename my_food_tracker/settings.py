f59b86792b1fac8d16ef014d0537524984c093d9
Wojurb07 <wojurbek@gmail.com>
2026-02-01 14:55:20 +0100

    modified etl execution file

--- my_food_tracker/settings.py
+++ my_food_tracker/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+# PostgreSQL
+import dj-database-url
+
 }
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- tracker/management/commands/run_product_base_etl.py
+++ tracker/management/commands/run_product_base_etl.py
@@ -2,15 +2,36 @@ from tracker.etl_product_base.extract import extract_product_base
 from tracker.etl_product_base.transform import transform_product_base
 from tracker.etl_product_base.load import load_product_base
 from django.core.management.base import BaseCommand
+import time
+import datetime
 
 class Command(BaseCommand):
     help = "Run ProductBase ETL pipeline. Data is scraped from Auchan site"
-    def handle(self, *args, **options):    
+    def handle(self, *args, **options):
+        # Extraction
+        extract_start = time.time()
         raw_data = extract_product_base("https://apps.auchan.pl/feed/getCard/feed")
+        extract_end = time.time()
+        extract_time = str(extract_end - extract_start)
+        self.stdout.write(f"Product Base extracted in, {extract_time} seconds.")
+        
+        # Transformation
+        transform_start = time.time()
         cleaned_data, invalid_products = transform_product_base(raw_data)
+        transform_end = time.time()
+        transform_time = str(transform_end - transform_start)
+        self.stdout.write(f"Product Base transformed in, {transform_time} seconds.")
         for product in invalid_products:
             self.stdout.write(f"Invalid product data: {product}")
+        
+        # Load
+        load_start = time.time()
         new_products, updated_products = load_product_base(cleaned_data)
+        load_end = time.time()
+        load_time = str(load_end - load_start)
+        self.stdout.write(f"Product Base loaded in, {load_time} seconds.")
+
+        # Final logs
         for new_product in new_products:
             self.stdout.write(f"New product added: {new_product}")
         for updated_product in updated_products:
